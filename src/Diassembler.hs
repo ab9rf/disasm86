c0a50bddb9623f46d0697f8fccfa3a8967e52614
Kelly Kinkade <kelly.lynn.martin@gmail.com>
2016-12-22 12:33:12 -0600

    Basic disassembly logic

--- src/Diassembler.hs
+++ src/Diassembler.hs
@@ -4,12 +4,16 @@ module Diassembler
         Instruction(..)
     ) where
 
+import Control.Monad (join)
 import Data.ByteString.Lazy (ByteString)
+import Data.Word (Word8)
+import Data.Binary.Get
+
 import qualified Data.ByteString.Lazy as B
 
 data Instruction = Instruction {
-      inPrefix :: [Prefix]
-    , inOpcode :: Opcode
+      inPrefix   :: [Prefix]
+    , inOpcode   :: Opcode
     , inOperands :: [Operand]
     }
 
@@ -19,6 +23,21 @@ data Opcode
 
 data Operand
 
-disassemble :: ByteString -> [Instruction]
-disassemble = const []
+disassemble :: ByteString -> ([Instruction], ByteString)
+disassemble s = case runGetOrFail disassemble1 s of
+                    Left _          -> ([], s)
+                    Right (r, _, i) -> let (i',r') = disassemble r in (i:i', r')
 
+disassemble1 :: Get Instruction
+disassemble1 =  disassemble1' pfxNone
+
+data PrefixState = PrefixState {
+    }
+
+pfxNone = PrefixState
+
+disassemble1' :: PrefixState -> Get Instruction
+disassemble1' pfx = do
+    byte <- getWord8
+    case byte of
+        _ -> fail ("invalid opcode " ++ show byte)
